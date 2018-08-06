f04a8ad021d7e0c42666360ddf68ec7a39439857
maimit <maimitp9@gmail.com>
2018-08-06 21:22:57 +0900

    update and delete inventory

--- features/step_definitions/book_inventory_steps.rb
+++ features/step_definitions/book_inventory_steps.rb
@@ -29,3 +29,34 @@ Then("I should see the new book in my inventory") do
   expect(page).to have_content('Redux')
   expect(page).to have_content('Pihu Patel')
 end
+
+Given("I have a book in my inventory") do
+  FactoryBot.create(:book, :user => @registered_user, :name => 'React.js', :author => 'Diyu')
+end
+
+When("I change the title of my book") do
+  visit root_path
+
+  click_link 'Edit'
+
+  fill_in 'book_name', with: 'Star Wars'
+
+  click_button 'Update Book'
+end
+
+Then("I should see the book with the new title in my inventory") do
+  visit root_path
+
+  expect(page).to_not have_content('React.js')
+  expect(page).to have_content('Diyu')
+end
+
+When("I remove a book from my inventory") do
+  visit root_path
+  click_link 'Destroy'
+end
+
+Then("I should not see it listing in the inventory anymore") do
+  expect(page).to_not have_content('React.js')
+end
+
--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -23,22 +23,22 @@ RSpec.describe BooksController, type: :controller do
 
   describe 'POST #create' do
     let(:book) { FactoryBot.build_stubbed(:book) }
-    let(:params) { { name: 'Test', authod: 'Sonu' } }
+    let(:params) { { name: 'Test', author: 'Sonu' } }
 
     before do
-      allow(:book).to receive(:save)
-      allow(:user).to receive_message_chain(:book, :build).and_return(book)
+      allow(book).to receive(:save)
+      allow(user).to receive_message_chain(:books, :build).and_return(book)
     end
 
     it 'saves the book' do
       post :create, params: { book: params }
-      expect(:book).to have_received(:save)
+      expect(book).to have_received(:save)
     end
 
     context 'when the book is succesfully saved' do
       before do
         allow(book).to receive(:save).and_return(true)
-        post :create, :params => { :book => params }
+        post :create, params: { book: params }
       end
 
       it 'redirects to the book show page' do
@@ -53,13 +53,72 @@ RSpec.describe BooksController, type: :controller do
     context 'when the book can’t be saved' do
       before do
         allow(book).to receive(:save).and_return(false)
-        post :create, :params => { :book => params }
+        post :create, params: { book: params }
       end
 
       it 'redirects back to the new page' do
         expect(response).to render_template(:new)
       end
     end
+  end
 
+  describe "PATCH #update" do
+    let(:book) { FactoryBot.build_stubbed(:book) }
+
+    before do
+      allow(Book).to receive(:find).and_return(book)
+      allow(book).to receive(:update).and_return(true)
+    end
+
+    it "updates the book" do
+      patch :update, :params => {
+        :id => book.id,
+        :book => { :name => 'New Name' } }
+      
+      expect(book).to have_received(:update)
+    end
+
+    context 'when the update succeds' do
+      before do
+        allow(book).to receive(:update).and_return(true)
+      end
+
+      it 'redirects to the book page' do
+        patch :update, :params => {
+          :id => book.id,
+          :book => { :name => 'New Name' } }
+        expect(response).to redirect_to(book_path(book))
+      end
+    end
+
+    context 'when the update fails' do
+      before do
+        allow(book).to receive(:update).and_return(false)
+      end
+
+      it 'renders the edit page again' do
+        patch :update, :params => {
+          :id => book.id,
+          :book => { :name => 'New Name' } }
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let(:book) { FactoryBot.build_stubbed(:book) }
+    before do
+      allow(Book).to receive(:find).and_return(book)
+      allow(book).to receive(:destroy)
+      delete :destroy, :params => { :id => book.id }
+    end
+
+    it "deletes the book" do
+      expect(book).to have_received(:destroy)
+    end
+
+    it "redirects to the index page" do
+      expect(response).to redirect_to(books_path)
+    end
   end
 end
